afcf1b6b5f39e3bc76a6f96fd559b0eb1c91edff
Mudasir Habib <mudasir.ee@hotmail.com>
2021-03-07 19:05:39 +0500

    updated

--- src/components/addOrders.jsx
+++ src/components/addOrders.jsx
@@ -6,6 +6,59 @@ const AddOrder = () => {
       <div className='add-order bg-white p-3'>
         <div className='card border-0'>
           <h6>Order Detail</h6>
+          {/* Stepper */}
+          <div className='w-100 stepper'>
+            <ul className='list-inline d-flex justify-content-between mx-5 '>
+              <li className='list-inline-item'>
+                <div className='step '>
+                  <i className='fa fa-check text-center align-middle'></i>
+                </div>
+                <span className='step-name align-middle'>
+                  <img
+                    src='assets/img/icons/Order page - Customer Stepper.svg'
+                    alt=''
+                  />
+                  Customer 1
+                </span>
+              </li>
+              <li className='list-inline-item'>
+                <div className='step '>
+                  <i className='fa fa-check text-center align-middle'></i>
+                </div>
+                <span className='step-name align-middle'>
+                  <img
+                    src='assets/img/icons/Order page - Customer Stepper.svg'
+                    alt=''
+                  />
+                  Customer 2
+                </span>
+              </li>
+              <li className='list-inline-item'>
+                <div className='step selected-step'>
+                  <i className='fa fa-book text-center align-middle'></i>
+                </div>
+                <span className='step-name align-middle'>
+                  <img
+                    src='assets/img/icons/Order page - Customer Stepper.svg'
+                    alt=''
+                  />
+                  Customer 3
+                </span>
+              </li>
+              <li className='list-inline-item'>
+                <div className='step  '>
+                  <i className='fa fa-check text-center align-middle'></i>
+                </div>
+                <span className='step-name text-end'>
+                  <img
+                    src='assets/img/icons/Order page - Customer Stepper.svg'
+                    alt=''
+                  />
+                  Customer 4
+                </span>
+              </li>
+            </ul>
+          </div>
           <h6>Select Items </h6>
 
           <div className='d-flex  w-100'>
@@ -116,28 +169,113 @@ const AddOrder = () => {
                 </div>
               </div>
             </div>
-
-            <div className='flex-fill p-2 selectedItems'>
+            {/* selected item */}
+            <div className='mx-3 selected'>
               <h6>Selected Items</h6>
-
-              <div className='d-inline-block'>
-                <img src='assets/img/icons/Order page - Image.svg' alt='' />
-              </div>
-              <div className='d-inline-block float-end'>
-                <span>Name</span>
-                <span>
-                  <img src='assets/img/icons/Order page - remove.svg' alt='' />
-                </span>
-              </div>
-              <div>
-                Color:
-                <select className='mx-3' name='color'>
-                  <option value=''>Navy Blue</option>
-                </select>
-                Size:
-                <select className='mx-3' name='size'>
-                  <option value=''>12.22mm</option>
-                </select>
+              <table className='selectedItems'>
+                <tbody>
+                  <tr>
+                    <td rowSpan='3'>
+                      <img
+                        src='assets/img/icons/Order page - Image.svg'
+                        alt=''
+                      />
+                    </td>
+                    <td>Name</td>
+                    <td colSpan='2' className='float-end'>
+                      <img
+                        src='assets/img/icons/Order page - remove.svg'
+                        alt=''
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>&nbsp;</td>
+                    <td colSpan='2'>Price SAR. 999</td>
+                  </tr>
+                  <tr>
+                    <td colSpan='3' className='float-end quantity'>
+                      <button className='btn btn-secondary'>+</button>1
+                      <button className='btn btn-secondary'>-</button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan='4'>
+                      {" "}
+                      <div>
+                        Color:
+                        <select className='mx-3' name='color'>
+                          <option value=''>Navy Blue</option>
+                        </select>
+                        Size:
+                        <select className='mx-3' name='size'>
+                          <option value=''>12.22mm</option>
+                        </select>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <hr />
+              <table className='selectedItems'>
+                <tbody>
+                  <tr>
+                    <td rowSpan='3'>
+                      <img
+                        src='assets/img/icons/Order page - Image.svg'
+                        alt=''
+                      />
+                    </td>
+                    <td>Name</td>
+                    <td colSpan='2' className='float-end'>
+                      <img
+                        src='assets/img/icons/Order page - remove.svg'
+                        alt=''
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>&nbsp;</td>
+                    <td colSpan='2'>Price SAR. 999</td>
+                  </tr>
+                  <tr>
+                    <td colSpan='3' className='float-end quantity'>
+                      <button className='btn btn-secondary'>+</button>1
+                      <button className='btn btn-secondary'>-</button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan='4'>
+                      {" "}
+                      <div>
+                        Color:
+                        <select className='mx-3' name='color'>
+                          <option value=''>Navy Blue</option>
+                        </select>
+                        Size:
+                        <select className='mx-3' name='size'>
+                          <option value=''>12.22mm</option>
+                        </select>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <div className='my-5'>
+                <span className='float-end'>Total Price: 1000</span>
+                <br />
+                <br />
+                <ul className='list-inline float-end'>
+                  <li className='list-inline-item'>
+                    <button className='btn btn-outline-dark'>Cancel</button>
+                  </li>
+                  <button className='btn btn-outline-secondary'>
+                    Previous
+                  </button>
+                  <li className='list-inline-item'></li>
+                  <button className='btn btn-secondary'>Next Step</button>
+                  <li className='list-inline-item'></li>
+                </ul>
               </div>
             </div>
           </div>
