7cd9281e321c6d648a8855779c6f26b05dd25d7d
Mudasir Habib <mudasir.ee@hotmail.com>
2021-03-07 20:03:11 +0500

    updated

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -6,7 +6,21 @@ const TableBody = ({orders, TableColumns}) => {
           return (
             <tr key={order._id}>
               {TableColumns.map((c) => {
-                return <td key={c.key}> {order[c.path] || c.content}</td>;
+                return c.path === "status" ? (
+                  <td key={c.key}>
+                    <span
+                      className={
+                        order[c.path] === "processing"
+                          ? "processing"
+                          : "delivering"
+                      }
+                    >
+                      {order[c.path] || c.content}
+                    </span>
+                  </td>
+                ) : (
+                  <td key={c.key}> {order[c.path] || c.content}</td>
+                );
               })}
             </tr>
           );
